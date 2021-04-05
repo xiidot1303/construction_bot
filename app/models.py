cc443fa5ff0de2c65a10f37acdd476661c4aa7ee
xiidot1303 <xii1303@inbox.ru>
2021-04-05 22:26:07 +0500

    new models

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Object(models.Model):
     title = models.CharField(null=True, blank=False, max_length=100)
     price = models.CharField(null=True, blank=False, max_length=30)
-    price_summ = models.CharField(null=True, blank=True, max_length=30)
-    price_dollar = models.CharField(null=True, blank=True, max_length=30)
+    #price_summ = models.CharField(null=True, blank=True, max_length=30)
+    #price_dollar = models.CharField(null=True, blank=True, max_length=30)
     def __str__(self):
         return self.title
 
