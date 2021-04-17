01bf51f65dc9ec22099fef658732a6d4806006a4
xiidot1303 <xii1303@inbox.ru>
2021-04-17 10:28:43 +0500

    add ModelAdmin

--- app/admin.py
+++ app/admin.py
@@ -2,10 +2,29 @@ from django.contrib import admin
 from app.models import *
 class ObjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'price')
+
+class ForemanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'login', 'account_summ', 'account_dollar')
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'title', 'summ_or_dollar', 'price', 'obj')
+
+
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'title', 'summ_or_dollar', 'price', 'obj')
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'login')
+
+class Bot_usersAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'who', 'login')
+
+class transfer_moneyAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'foreman', 'object', 'transfered')
 admin.site.register(Object, ObjectAdmin)
-admin.site.register(Foreman)
-admin.site.register(Material)
-admin.site.register(Salary)
-admin.site.register(Client)
-admin.site.register(Bot_users)
-admin.site.register(transfer_money)
+admin.site.register(Foreman, ForemanAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(Salary, SalaryAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Bot_users, Bot_usersAdmin)
+admin.site.register(transfer_money, transfer_moneyAdmin)
